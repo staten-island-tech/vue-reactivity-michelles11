bf6fe243ca75b4191f6d7d1646bf8506c2633e48
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-03-02 09:54:16 -0500

    123

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <h1>{{ student }}</h1>
+</template>
+<ul> 
+<li v-for="animal in animals" :key="animal">{{ animal }}</li>
+</ul>
+</template>
+</script>
+export default {
+  data(){
+    return {
+      student: "Jason",
+      animals:["pig, "cow", "ostrich", "Zee"]
+    }
+  }
+}
