34f32666c9e8e8a29d777d420de03ae2cbc53098
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-03-08 09:35:48 -0500

    s

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,17 +1,24 @@
 
 <template>
-  <h1>{{ student }}</h1>
-</template>
+  <h1 v-if ="graduated">{{ student }}</h1>
+  <h1 v-else>"They have not yet graduated"</h1>
 <ul> 
 <li v-for="animal in animals" :key="animal">{{ animal }}</li>
 </ul>
+<input :value=  />
 </template>
 </script>
 export default {
   data(){
     return {
       student: "Jason",
-      animals:["pig, "cow", "ostrich", "Zee"]
+      animals:["pig", "cow", "ostrich", "Zee"]
     }
+  };
+},
+methods: {
+  onInput(e) {
+    this.text = e.target.value;
   }
 }
+</script>
