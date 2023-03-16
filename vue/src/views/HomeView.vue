bb534056df64979c193712e751bbd672f4af38de
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-03-16 10:16:11 -0400

    Update HomeView.vue

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup>
 import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <TheWelcome />
+    <div class="home">
+      <button v-if="loggedIn">Log Out</button>
+      <button v-else>Login</button>
   </main>
 </template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      student: 'Harry',
+      graduated: true,
+      animals: ['Pig', 'Horse', 'Donkey', 'Cow', 'Duck']
+      //animals
+    }
+  }
+}
+</script>
+<style></style>
