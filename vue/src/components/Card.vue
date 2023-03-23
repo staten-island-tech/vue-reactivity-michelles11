bef829ac03262916f13db029b8510282511b2b94
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-03-23 10:15:31 -0400

    very confused

--- vue/src/components/Card.vue
+++ vue/src/components/Card.vue
@@ -1,9 +1,37 @@
 <template>
-  <div></div>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <img v-bind:src="image" alt="" />
+    <p>{{ description }}</p>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Card',
+  props: {
+    title: String,
+    description: String,
+    image: String
+  }
+}
 </script>
 
-<style scoped></style>
+<style>
+.card {
+  width: 200px;
+  padding: 10px;
+  text-align: center;
+  border-radius: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: antiquewhite;
+  height: 500px;
+  margin: 10px auto;
+}
+.image {
+  height: 30vh;
+  border-radius: 1rem;
+}
+</style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -4,19 +4,42 @@ import TheWelcome from '../components/TheWelcome.vue'
   <div class="home">
     <Button @button-click="bye">Button 1</Button>
     <Button @button-click="hello">Button 2</Button>
+    <Card
+      title="Hello World"
+      description="blah blah blah "
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Vulpes_vulpes_ssp_fulvus.jpg/1200px-Vulpes_vulpes_ssp_fulvus.jpg"
+    ></Card>
   </div>
 </template>
 
 <script>
 import Button from '../components/Button.vue'
+import Card from '../components/Card.vue'
 export default {
   name: 'Home',
   components: {
-    Button
+    Button,
+    Card
   },
   data() {
     return {
-      selected: ''
+      tvshows: [
+        'Gilmore Girls',
+        'Friends',
+        'The Office',
+        'The Vampire Diaries',
+        'How I Met Your Mother',
+        'Breaking Bad',
+        'Arrested Development',
+        'Outer Banks',
+        'Ted Lasso',
+        'Shrinking',
+        'A Series of Unfortunate Events',
+        'Jane The Virgin',
+        'Emily In Paris',
+        'Gossip Girl',
+        ''
+      ]
     }
   },
 
